6243a02858817afbbc4eae431562a8f34f5893a4
lamvanchi <109412135+lamvanchi@users.noreply.github.com>
2026-09-07 22:07:59 +0700

    DELETE

--- scr/Services/IOrderService.cs
+++
@@ -1,9 +0,0 @@
-using AppleStore.Models;
-using AppleStore.ViewModels;
-
-namespace AppleStore.Services;
-
-public interface IOrderService
-{
-    Task<(bool Success, string Message, Order? Order)> PlaceOrderAsync(string userId, CheckoutViewModel input, CancellationToken cancellationToken = default);
-}
